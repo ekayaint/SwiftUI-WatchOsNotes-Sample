32a7e0cc5bbb48a9a865dd3a0ec5f32e42b5e046
ekayaint <ekayaint@gmail.com>
2023-09-10 15:48:21 +0300

    Initial Commit

---
+++ SwiftUI-WatchOsNotes-Sample Watch App/SwiftUI_WatchOsNotes_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WatchOsNotes_SampleApp.swift
+//  SwiftUI-WatchOsNotes-Sample Watch App
+//
+//  Created by ekayaint on 10.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WatchOsNotes_Sample_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
