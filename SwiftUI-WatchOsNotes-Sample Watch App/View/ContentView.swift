d598c166d8ee84993911c1b6143820c5e991aea5
ekayaint <ekayaint@gmail.com>
2023-09-10 19:20:31 +0300

    data saved

--- SwiftUI-WatchOsNotes-Sample Watch App/View/ContentView.swift
+++ SwiftUI-WatchOsNotes-Sample Watch App/View/ContentView.swift
@@ -11,13 +11,49 @@ struct ContentView: View {
     @State private var notes: [Note] = [Note]()
     @State private var text: String = ""
     
+    func getDocumentDirectory() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
+    }
+    
+    func save() {
+        //dump(notes)
+        do {
+            let data = try JSONEncoder().encode(notes)
+            
+            let url = getDocumentDirectory().appendingPathComponent("notes")
+            
+            try data.write(to: url)
+        } catch {
+            print("Savinf data has failed")
+        }
+    }
+    
+    func load() {
+        DispatchQueue.main.async {
+            do {
+                let url = getDocumentDirectory().appendingPathComponent("notes")
+                
+                let data = try Data(contentsOf: url)
+                
+                notes = try JSONDecoder().decode([Note].self, from: data)
+            } catch {
+                // Do noting
+            }
+        }
+    }
+    
     var body: some View {
          
         VStack {
             HStack(alignment: .center, spacing: 6) {
                 TextField("Add New Note", text: $text)
                 Button {
-                    // Action
+                    guard text.isEmpty == false else { return }
+                    let note = Note(id: UUID(), text: text)
+                    notes.append(note)
+                    text = ""
+                    save()
                 } label: {
                     Image(systemName: "plus.circle")
                         .font(.system(size: 42, weight: .semibold))
@@ -28,7 +64,11 @@ struct ContentView: View {
                 //.buttonStyle(BorderedButtonStyle(tint: .accentColor))
             } //: HStack
             Spacer()
+            Text("\(notes.count)")
         } //: VStack
+        .onAppear(perform: {
+            load()
+        })
         .navigationTitle("Notes")
         
     }
